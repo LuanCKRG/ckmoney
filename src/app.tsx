1684429762f70cc51c9135ae020450b3182911a0
LuanCKRG <luancamposck@gmail.com>
2025-04-03 18:49:18 -0300

    feat(app): add transaction table with sample data and structure

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,17 @@ import incomeImg from "@/assets/income.svg"
 import outcomeImg from "@/assets/outcome.svg"
 import totalImg from "@/assets/total.svg"
 
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+
+interface TransactionProps {
+	id: number
+	value: number
+	title: string
+	type: "income" | "outcome"
+	date: string // No formato DD/MM/YYYY
+	category: string
+}
+
 export const App = () => {
 	const cards = [
 		{
@@ -17,6 +28,89 @@ export const App = () => {
 		{
 			title: "Total",
 			image: totalImg
+		}
+	]
+
+	const transactions: TransactionProps[] = [
+		{
+			id: 1,
+			value: 3500,
+			title: "Salário",
+			type: "income",
+			date: "15/03/2024",
+			category: "Trabalho"
+		},
+		{
+			id: 2,
+			value: 200,
+			title: "Aluguel",
+			type: "outcome",
+			date: "10/03/2024",
+			category: "Casa"
+		},
+		{
+			id: 3,
+			value: 1200,
+			title: "Freelance",
+			type: "income",
+			date: "12/03/2024",
+			category: "Trabalho"
+		},
+		{
+			id: 4,
+			value: 450,
+			title: "Supermercado",
+			type: "outcome",
+			date: "14/03/2024",
+			category: "Alimentação"
+		},
+		{
+			id: 5,
+			value: 800,
+			title: "Venda Bike",
+			type: "income",
+			date: "08/03/2024",
+			category: "Lazer"
+		},
+		{
+			id: 6,
+			value: 90,
+			title: "Internet",
+			type: "outcome",
+			date: "05/03/2024",
+			category: "Casa"
+		},
+		{
+			id: 7,
+			value: 75,
+			title: "Cinema",
+			type: "outcome",
+			date: "17/03/2024",
+			category: "Lazer"
+		},
+		{
+			id: 8,
+			value: 35,
+			title: "Uber",
+			type: "outcome",
+			date: "13/03/2024",
+			category: "Transporte"
+		},
+		{
+			id: 9,
+			value: 300,
+			title: "Curso Online",
+			type: "outcome",
+			date: "01/03/2024",
+			category: "Educação"
+		},
+		{
+			id: 10,
+			value: 15,
+			title: "Cafeteria",
+			type: "outcome",
+			date: "19/03/2024",
+			category: "Alimentação"
 		}
 	]
 
@@ -43,6 +137,28 @@ export const App = () => {
 						</div>
 					))}
 				</div>
+
+				<Table className="mt-16">
+					<TableHeader>
+						<TableRow>
+							<TableHead className="w-[100px]">Título</TableHead>
+							<TableHead>Valor</TableHead>
+							<TableHead>category</TableHead>
+							<TableHead>Data</TableHead>
+						</TableRow>
+					</TableHeader>
+
+					<TableBody>
+						{transactions.map((transaction) => (
+							<TableRow key={transaction.id}>
+								<TableCell className="font-medium">{transaction.title}</TableCell>
+								<TableCell>{transaction.value}</TableCell>
+								<TableCell>{transaction.date}</TableCell>
+								<TableCell className="text-right">{transaction.date}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
 			</div>
 		</>
 	)
