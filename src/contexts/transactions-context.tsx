0b620c7526f96687cf1fd110f4b3d46bd25adaf7
LuanCKRG <luancamposck@gmail.com>
2025-04-05 23:57:21 -0300

    feat(transactions): add TransactionsContext and provider for managing transactions

---
+++ src/contexts/transactions-context.tsx
@@ -0,0 +1,48 @@
+import type { TransactionProps } from "@/types/transaction-type"
+import { createContext, useContext, useState, useEffect, type ReactNode } from "react"
+
+interface TransactionsContextData {
+	transactions: TransactionProps[]
+	addTransaction: (transaction: Omit<TransactionProps, "id">) => void
+	removeTransaction: (id: number) => void
+}
+
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
+
+export const TransactionsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+	const [transactions, setTransactions] = useState<TransactionProps[]>(() => {
+		if (typeof window !== "undefined") {
+			const storedTransactions = localStorage.getItem("transactions")
+			return storedTransactions ? JSON.parse(storedTransactions) : []
+		}
+		return []
+	})
+
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			localStorage.setItem("transactions", JSON.stringify(transactions))
+		}
+	}, [transactions])
+
+	const addTransaction = (transaction: Omit<TransactionProps, "id">) => {
+		const newTransaction = {
+			...transaction,
+			id: Date.now()
+		}
+		setTransactions((prev) => [...prev, newTransaction])
+	}
+
+	const removeTransaction = (id: number) => {
+		setTransactions((prev) => prev.filter((transaction) => transaction.id !== id))
+	}
+
+	return <TransactionsContext.Provider value={{ transactions, addTransaction, removeTransaction }}>{children}</TransactionsContext.Provider>
+}
+
+export const useTransactions = () => {
+	const context = useContext(TransactionsContext)
+	if (!context) {
+		throw new Error("useTransactions must be used within a TransactionsProvider")
+	}
+	return context
+}
