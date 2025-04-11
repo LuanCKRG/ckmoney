3e98823117759fbc16585de7792d22fe536c68ed
LuanCKRG <luancamposck@gmail.com>
2025-04-11 16:56:42 -0300

    feat(app): replace card display with Summary component for income, outcome, and total

--- src/app.tsx
+++ src/app.tsx
@@ -1,50 +1,16 @@
 import { Header } from "@/components/header"
 import { TransactionsTable } from "@/components/transactions-table"
 
-import incomeImg from "@/assets/income.svg"
-import outcomeImg from "@/assets/outcome.svg"
-import totalImg from "@/assets/total.svg"
 import { TransactionsProvider } from "@/contexts/transactions-context"
+import { Summary } from "./components/summary"
 
 export const App = () => {
-	const cards = [
-		{
-			title: "Entradas",
-			image: incomeImg
-		},
-		{
-			title: "Saídas",
-			image: outcomeImg
-		},
-		{
-			title: "Total",
-			image: totalImg
-		}
-	]
-
 	return (
 		<TransactionsProvider>
 			<Header />
 
 			<div className="mx-auto max-w-6xl py-10 px-4">
-				<div className="-mt-30 flex flex-col md:grid md:grid-cols-3 gap-5">
-					{cards.map((card) => (
-						<div key={card.title} className="bg-shape px-6 py-3 rounded-sm text-title shadow-lg">
-							<header className="flex items-center justify-between">
-								<p>{card.title}</p>
-								<img src={card.image} alt="Entradas" />
-							</header>
-
-							<strong className="mt-4 text-3xl font-medium leading-12">
-								{" "}
-								{Intl.NumberFormat("pt-BR", {
-									style: "currency",
-									currency: "BRL"
-								}).format(120)}{" "}
-							</strong>
-						</div>
-					))}
-				</div>
+				<Summary />
 
 				<TransactionsTable className="mt-16" />
 			</div>
