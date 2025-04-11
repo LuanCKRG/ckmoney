576121ffced0c1bb2a93593076b4b09b648374b6
LuanCKRG <luancamposck@gmail.com>
2025-04-11 16:56:54 -0300

    fix(transactions-table): remove unused PopoverDemo import

--- src/components/transactions-table.tsx
+++ src/components/transactions-table.tsx
@@ -3,7 +3,6 @@ import { useTransactions } from "@/contexts/transactions-context"
 import { cn } from "@/lib/utils"
 import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
-import { PopoverDemo } from "./category-popover"
 
 // const TRANSACTIONS: TransactionProps[] = [
 // 	{
