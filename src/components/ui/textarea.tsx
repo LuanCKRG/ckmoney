b266af5cd185aae79c046a1ef1f4286e846e0a61
LuanCKRG <luancamposck@gmail.com>
2025-04-15 11:15:42 -0300

    feat(textarea): add Textarea component from shadcn

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,18 @@
+import type * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Textarea = ({ className, ...props }: React.ComponentProps<"textarea">) => {
+	return (
+		<textarea
+			data-slot="textarea"
+			className={cn(
+				"border-neutral-200 placeholder:text-neutral-500 focus-visible:border-light-purple focus-visible:ring-light-purple/50 aria-invalid:ring-red-500/20 aria-invalid:border-red-500 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+				className
+			)}
+			{...props}
+		/>
+	)
+}
+
+export { Textarea }
